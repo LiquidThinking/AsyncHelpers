b6f648d1215595495106f136179bbb71536283eb
Jake Rote <jrote1@me.com>
2017-08-31 11:54:24 +0100

    Added async lock with key and AsValueTask extension method

---
+++ AsyncHelpers/GenericExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncHelpers
+{
+	public static class GenericExtensions
+	{
+		public static ValueTask<T> AsValueTask<T>( this T obj )
+		{
+			return new ValueTask<T>( obj );
+		}
+	}
+}
