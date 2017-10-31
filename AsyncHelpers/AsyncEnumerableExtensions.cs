3bb6798cba79af1eddfea6c4760bc8557db8e7d5
Jack Herring <jtherring@hotmail.co.uk>
2017-10-31 16:46:32 +0000

    Added async lock

---
+++ AsyncHelpers/AsyncEnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncHelpers
+{
+    public static class AsyncEnumerableExtensions
+    {
+        public static async Task<List<T>> ToListAsync<T>( this IEnumerable<Task<T>> list )
+        {
+            var items = list.ToList();
+            await Task.WhenAll( items );
+            return items.Select( x => x.Result ).ToList();
+        }
+    }
+}
